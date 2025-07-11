56bedbca189b2bdfc98c0b9904c9b9624b33f1d0
KRojowsky <rojowsky@poczta.fm>
2025-07-11 09:20:59 +0200

    Adding two main sections

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import MainSectionFirst from './components/MainSectionFirst/MainSectionFirst';
+import MainSectionSecond from './components/MainSectionSecond/MainSectionSecond';
+
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <MainSectionFirst />
+      <MainSectionSecond />
     </>
   );
 };
---
+++ frontend/src/components/MainSectionSecond/MainSectionSecond.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './MainSectionSecond.scss';
+import road from '../../assets/images/main-section-2/road.png';
+
+
+const points = [
+    {
+        title: "Dodawanie zgłoszeń",
+        description: "Użytkownicy mogą zgłaszać problemy infrastrukturalne, określając ich kategorię, lokalizację oraz szczegóły dotyczące awarii."
+    },
+    {
+        title: "Obsługa i zarządzanie zgłoszeniami",
+        description: "Odpowiednie jednostki administracyjne otrzymują zgłoszenia, analizują je i przypisują do realizacji."
+    },
+    {
+        title: "Priorytetyzacja problemów",
+        description: "Mieszkańcy mogą wspierać zgłoszenia poprzez ich ocenianie, co pozwala na efektywniejsze ustalanie priorytetów interwencji."
+    },
+    {
+        title: "Wizualizacja na mapie",
+        description: "Zgłoszenia są prezentowane na interaktywnej mapie, co ułatwia identyfikację problemów w danym regionie."
+    }
+]
+
+const MainSectionSecond: React.FC = () => {
+    return (
+        <section className='how-it-works'>
+            <h2 className="how-it-works-title">Jak działa system zgłaszania problemów infrastrukturalnych?</h2>
+
+            <div className="how-it-works-content">
+                <ul className='how-it-works-points'>
+                    {points.map((point, index) => (
+                        <li key={index} className="how-it-works-point">
+                            <span className="point-number">{index + 1}</span>
+                            <div className="point-texts">
+                                <h3>{point.title}</h3>
+                                <p>{point.description}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="how-it-works-image">
+                    <img src={road} alt="Jak działa system" />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MainSectionSecond;
