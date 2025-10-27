c5c81e3bc738aad5baae1d74966ccde903931d70
KRojowsky <rojowsky@poczta.fm>
2025-10-27 10:27:15 +0100

    Update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import DashboardPage from './pages/DashboardPage';
 import ProfilePage from './pages/ProfilePage';
-import Login from './pages/Login'; // <-- dodaj import strony logowania
+import Login from './pages/Login';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />          {/* <-- route do logowania */}
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
